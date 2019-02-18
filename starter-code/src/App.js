57f1c84c8125ce0deaf2bc9921387b3722f95879
hendrik <hendrik@avocando.com>
2019-02-18 19:55:39 +0100

    new stuff

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bulma/css/bulma.css';
 import './App.css';
+
+import foods from './foods.json'
+
+import FoodBox from './components/FoodBox'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {foods.map((food, idx) => {
+          return <FoodBox key={idx} image={food.image} calories={food.calories} name={food.name}/>
+        })}
       </div>
     );
   }
